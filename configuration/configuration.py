6e099efefd59c798736116efea1e2a1f41aa9a0f
Vasia Kalaitzi <kalaitzivasia@gmail.com>
2022-04-18 18:00:21 +0300

    Update configuration.py

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -7,8 +7,8 @@ query_suffix1='''`;'''
 #----------------------
 #BigQuery_to_SQL_submit configurations
 
-query2= """SELECT fullVisitorId AS Customer_Id, visitStartTime AS First_Session_Timestamp,	Total_Conv_Time	FROM (	SELECT fullVisitorId,visitStartTime,SUM(totals.timeOnSite) OVER (PARTITION BY fullVisitorId) AS Total_Conv_Time,ROW_NUMBER() OVER (PARTITION BY fullVisitorId ORDER BY visitStartTime ASC) rn FROM 	`bigquery-public-data.google_analytics_sample.ga_sessions_"""
-query_suffix2="""` ) WHERE	rn=1;"""
+query2= """SELECT fullVisitorId AS Customer_Id, visitStartTime AS First_Session_Timestamp,Total_Conv_Time	FROM (SELECT fullVisitorId,visitStartTime,SUM(totals.timeOnSite) OVER (PARTITION BY fullVisitorId) AS Total_Conv_Time,ROW_NUMBER() OVER (PARTITION BY fullVisitorId ORDER BY visitStartTime ASC) rn FROM	`bigquery-public-data.google_analytics_sample.ga_sessions_"""
+query_suffix2="""` ) WHERE rn=1;"""
 
 export_path='/BigQuery_processing/datasets/extracted_data.csv'
 
